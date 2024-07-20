1ae45084a941555f8d5aba79c02c0e4c12148265
Oscar Ruiz <oscarruiz2614@gmail.com>
2024-07-19 22:54:23 -0500

    feat: :sparkles: users crud

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+    constructor(@InjectRepository(User) private userRepository: Repository<User>) { }
+
+    create(user: CreateUserDto): Promise<User> {
+        const newUser = this.userRepository.create(user);
+        return this.userRepository.save(newUser);
+    }
+
+    all(): Promise<User[]> {
+        return this.userRepository.find();
+    }
+
+    find(id: number): Promise<User> {
+        return this.userRepository.findOne({ where: { id } });
+    }
+}
